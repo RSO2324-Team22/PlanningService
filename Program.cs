081ff378ebcbf078e7cde9ae464122029908e43f
Jure Pustoslemšek <jure.poglednik@gmail.com>
2023-12-06 13:41:45 +0100

    DbContext, postgres

--- Program.cs
+++ Program.cs
@@ -1,14 +1,21 @@
+using Microsoft.EntityFrameworkCore;
 using PlanningService.Database;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+string postgres_server = builder.Configuration["POSTGRES_SERVER"] ?? "";
+string postgres_database = builder.Configuration["POSTGRES_DATABASE"] ?? "";
+string postgres_username = builder.Configuration["POSTGRES_USERNAME"] ?? "";
+string postgres_password = builder.Configuration["POSTGRES_PASSWORD"] ?? "";
 
+// Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<PlanningDbContext>();
+builder.Services.AddDbContext<PlanningDbContext>(options => {
+    options.UseNpgsql($"Host={postgres_server};Username={postgres_username};Password={postgres_password};Database={postgres_database}");
+});
 
 var app = builder.Build();
 
