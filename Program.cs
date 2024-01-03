f13a9a6aae178364c67dee848d914583011e24a5
Matrkorac <matic.koracin@gmail.com>
2024-01-03 01:45:26 +0100

    Exposed swagger at "openapi" endpoint

--- Program.cs
+++ Program.cs
@@ -29,11 +29,11 @@ builder.Services.AddHealthChecks()
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwaggerUI(options => {
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = "openapi";
+    options.DocumentTitle = "OpenAPI documentation";
+});
 
 app.UseHttpsRedirection();
 
