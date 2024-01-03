8627ec2d1b483bc8e0c1aa8c57877e51c8df5607
Matrkorac <matic.koracin@gmail.com>
2024-01-03 23:28:04 +0100

    Fixed swagger configuration

--- Program.cs
+++ Program.cs
@@ -29,6 +29,7 @@ builder.Services.AddHealthChecks()
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseSwagger();
 app.UseSwaggerUI(options => {
     options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
     options.RoutePrefix = "openapi";
